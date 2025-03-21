6dbbf4772500dec7a8853cbb36e71209cd7c87ae
CHANDRUS-HUB <chandrumani72gmail.com>
2025-03-21 09:52:24 +0530

    complete

--- product-management-MySql/server/Models/Userdb.js
+++ product-management-MySql/server/Models/Userdb.js
@@ -3,9 +3,7 @@ const sequelize = require("../config/userdata");
 
 const UserDB = sequelize.define("User", {
     name: { type: DataTypes.STRING, allowNull: false ,
-        validate: { 
-            len: [1, 10] 
-        }
+      
     },
     email: { type: DataTypes.STRING, allowNull: false, unique: true },
     password: { type: DataTypes.STRING, allowNull: false }
